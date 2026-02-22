8a50d092ccedde84a1d3e8318cb81a3cb2e300ad
Smruthi Nayak <smritinayak2006@gmail.com>
2026-02-22 17:20:37 +0530

    Day 2: Added conditionals and calculator logic

---
+++ D1.py
@@ -0,0 +1,16 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+color = input("Enter your favorite color: ")    
+
+print("Hello",name)
+print("You are",age,"years old.")
+print("Next year, you will be",age + 1)
+print("Smriti is",age,"years old and likes",color)
+
+print("Welcome to my first backend simulation")
+
+user_input = input("Type something: ")
+
+print("Server received:", user_input)
+print("Server response: Thank you for your message")
+
