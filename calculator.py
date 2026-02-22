815fb36e8c3889f5a6eeb871a1ee1c8094fea7d0
Smruthi Nayak <smritinayak2006@gmail.com>
2026-02-22 17:33:43 +0530

    Day 3 : Added loops and repeat calculator functionality

--- calculator.py
+++ calculator.py
@@ -1,30 +1,37 @@
 print("Simple Calculator")
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+while True:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
 
-print("Select operation:")
-print("1. Add")     
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
+    print("Choose operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
 
-choice = input("Enter choice (1/2/3/4): ")
+    choice = input("Enter choice (1/2/3/4): ")
 
-if choice == '1':
-    print("Result: ", num1 + num2)
-          
-elif choice == '2':
-    print("Result: ", num1 - num2)
+    if choice == "1":
+        print("Result:", num1 + num2)
 
-elif choice == '3':
-    print("Result: ", num1 * num2)
+    elif choice == "2":
+        print("Result:", num1 - num2)
 
-elif choice == '4':
-    if num2 != 0:
-        print("Result: ", num1 / num2)
+    elif choice == "3":
+        print("Result:", num1 * num2)
+
+    elif choice == "4":
+        if num2 != 0:
+            print("Result:", num1 / num2)
+        else:
+            print("Error: Cannot divide by zero")
+
     else:
-        print("Error: Division by zero is not allowed.")
+        print("Invalid choice")
 
-else:
-    print("Invalid input")
+    again = input("Do you want to calculate again? (yes/no): ")
+
+    if again.lower() != "yes":
+        print("Goodbye!")
+        break
