4f321bc4d23f1d705d7a62377a7215a6172a0724
Smruthi Nayak <smritinayak2006@gmail.com>
2026-03-16 08:48:39 +0530

    Day 6: Added input validation and fixed calcutator structure

--- calculator.py
+++ calculator.py
@@ -12,12 +12,20 @@ def divide(a, b):
         return a / b
     else:
         return "Error: Cannot divide by zero"
-    
+
+
 history = []
 
 while True:
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+
+    # Number validation
+    while True:
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter numbers only.")
 
     print("Choose operation:")
     print("1. Add")
@@ -25,34 +33,43 @@ while True:
     print("3. Multiply")
     print("4. Divide")
 
-    choice = input("Enter choice (1/2/3/4): ")
+    # Choice validation
+    while True:
+        choice = input("Enter choice (1/2/3/4): ")
+
+        if choice in ["1", "2", "3", "4"]:
+            break
+        else:
+            print("Invalid choice. Please select 1, 2, 3, or 4.")
 
     if choice == "1":
         result = add(num1, num2)
-        print("Result:", result)
         history.append(f"{num1} + {num2} = {result}")
 
     elif choice == "2":
         result = subtract(num1, num2)
-        print("Result:", result)
         history.append(f"{num1} - {num2} = {result}")
 
     elif choice == "3":
         result = multiply(num1, num2)
-        print("Result:", result)
         history.append(f"{num1} * {num2} = {result}")
 
     elif choice == "4":
         result = divide(num1, num2)
-        print("Result:", result)
         history.append(f"{num1} / {num2} = {result}")
 
-    else:
-        print("Invalid choice")
+    print("Result:", result)
 
-    again = input("Do you want to calculate again? (yes/no): ")
+    # Repeat validation
+    while True:
+        again = input("Do you want to calculate again? (yes/no): ").lower()
 
-    if again.lower() != "yes":
+        if again in ["yes", "no"]:
+            break
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+    if again == "no":
         print("\nCalculation History:")
         for item in history:
             print(item)
